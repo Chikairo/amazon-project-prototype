f8afe1815f7194f9d7c2a9733e597e46f493c862
Kairo <blessingchikairo@gmail.com>
2025-02-13 00:32:02 +0100

    initial commit

---
+++ data/deliveryoption.js
@@ -0,0 +1,25 @@
+export const deliveryOptions = [{
+    id: "1",
+    deliveryDays: 7,
+    priceInCents: 0,
+}, {
+    id: "2",
+    deliveryDays: 3,
+    priceInCents: 499,
+}, {
+    id: "3",
+    deliveryDays: 1,
+    priceInCents: 999,
+}];
+
+export function getDeliveryOption(deliveryOptionId) {
+    let deliveryOption;
+
+    deliveryOptions.forEach((option) => {
+        if (option.id === deliveryOptionId) {
+            deliveryOption = option;
+        }
+    });
+
+    return deliveryOption || deliveryOption[0];
+};
